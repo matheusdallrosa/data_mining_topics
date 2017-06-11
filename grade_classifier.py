4be161d8d3973f3be54abf6c22c52f486c622507
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-06-11 10:05:09 -0300

    We need to choose a better training set.

--- grade_classifier.py
+++ grade_classifier.py
@@ -3,53 +3,56 @@ import sys
 import numpy as np
 from sklearn import tree
 
-reputation = {'"home"':1,'"reputation"':2,'"course"':3,'"other"':4}
+train_features = []
+train_targets = []
+with open(sys.argv[1]) as f:
+    for row in f:
+        info = row.split(';')
+        train_targets.append(info[32])
+        train_features.append(info[:30])        
 
-def convert_float(data):
-    if type(data) == float or type(data) == int: return data
-    if data == '"U"' or data == '"T"' or data == '"no"': return 0
-    if data == '"R"' or data == '"A"' or data == '"yes"': return 1
-    if data in reputation: return reputation[data]
-    return data
+test_features = []
+test_targets = []
+with open(sys.argv[2]) as f:
+    for row in f:
+        info = row.split(';')
+        test_targets.append(info[32])
+        test_features.append(info[:30])
 
-base = []
-for row in sys.stdin:
-    base.append(row)
 
-useless_features = [0,1,4,8,9,11]
-features, targets = [], []
+max_score = 0
+max_score_features = []
+for i in range(0,30):
+    for j in range(i+1,30):    
+        for k in range(j+1,30):
+            #for l in range(k+1,30):
+            #    for m in range(l+1,30):
+            #        for n in range(m+1,30):
+                        #useless_features = [0,1,4,8,9,11]
+                        
+            useless_features = []
+            for z in range(0,30):
+                if z != i and z != j and z != k:# and z != l and z != m and z != n: 
+                    useless_features.append(z)                        
+                    
+            usefull_train_features = []     
+            for student_info in train_features:                                
+                #features of the given student.
+                usefull_features = np.delete(student_info[:30],useless_features)                            
+                usefull_train_features.append(usefull_features)                            
 
-check_train = 0
-test_idx = {1,51,101,151,201,251,301,351,401,451,501,551,601}
-test_features, test_targets = [], []
-for student in base:
-    #to check if this student is for tests.
-    check_train += 1
-    #the input row came from a csv file.
-    student_info = student.split(';')
+            usefull_test_features = []
+            for student_info in test_features:
+                usefull_features = np.delete(student_info[:30],useless_features)
+                usefull_test_features.append(usefull_features)                            
 
-    #features of the given student.
-    usefull_features_str = np.delete(student_info[:30],useless_features)
-    usefull_features_numeric = []
-    for f in usefull_features_str:        
-        usefull_features_numeric.append(convert_float(f)) 
-    
-    if check_train in test_idx:
-        test_features.append(usefull_features_numeric)
-        test_targets.append(student_info[32])
-    else:
-        features.append(usefull_features_numeric)
-
-        #grades of the given student.
-        #for g in student_info[30:33]:
-        #  print(g)
-        #  targets.append(float(g.split('"')[1]))
-
-        #first we'll focus on the final grade.
-        targets.append(student_info[32])
-
-clf = tree.DecisionTreeClassifier()
-clf.fit(features,targets)
-print(clf.score(test_features,test_targets))
-print(test_targets)
-print(clf.predict(test_features,test_targets))
+            clf = tree.DecisionTreeClassifier(random_state=1000000007,min_samples_split=2)
+            clf.fit(usefull_train_features,train_targets)
+            score = clf.score(usefull_test_features,test_targets)*100
+            #print(str(i)+" "+str(score))
+            if score > max_score:
+                max_score = score
+                max_score_features = [i,j,k]
+            print(str(max_score))                    
+print(max_score)
+print(max_score_features)
