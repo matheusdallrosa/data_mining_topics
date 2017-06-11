4b391bc82f0d374830c2f566f6a719dc6bcd9a6c
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-06-11 12:53:13 -0300

    Looking for good features.

--- grade_classifier.py
+++ grade_classifier.py
@@ -25,34 +25,35 @@ max_score_features = []
 for i in range(0,30):
     for j in range(i+1,30):    
         for k in range(j+1,30):
-            #for l in range(k+1,30):
-            #    for m in range(l+1,30):
-            #        for n in range(m+1,30):
-                        #useless_features = [0,1,4,8,9,11]
+            for l in range(k+1,30):
+                    #    for m in range(l+1,30):
+                    #        for n in range(m+1,30):
+                                #useless_features = [0,1,4,8,9,11]
+                                
+                useless_features = []
+                for z in range(0,30):
+                    if z != i and z != j and z != k and z != l:# and z != m and z != n: 
+                        useless_features.append(z)                        
                         
-            useless_features = []
-            for z in range(0,30):
-                if z != i and z != j and z != k:# and z != l and z != m and z != n: 
-                    useless_features.append(z)                        
-                    
-            usefull_train_features = []     
-            for student_info in train_features:                                
-                #features of the given student.
-                usefull_features = np.delete(student_info[:30],useless_features)                            
-                usefull_train_features.append(usefull_features)                            
+                usefull_train_features = []     
+                for student_info in train_features:                                
+                    #features of the given student.
+                    usefull_features = np.delete(student_info[:30],useless_features)                            
+                    usefull_train_features.append(usefull_features)                            
 
-            usefull_test_features = []
-            for student_info in test_features:
-                usefull_features = np.delete(student_info[:30],useless_features)
-                usefull_test_features.append(usefull_features)                            
+                usefull_test_features = []
+                for student_info in test_features:
+                    usefull_features = np.delete(student_info[:30],useless_features)
+                    usefull_test_features.append(usefull_features)                            
 
-            clf = tree.DecisionTreeClassifier(random_state=1000000007,min_samples_split=2)
-            clf.fit(usefull_train_features,train_targets)
-            score = clf.score(usefull_test_features,test_targets)*100
-            #print(str(i)+" "+str(score))
-            if score > max_score:
-                max_score = score
-                max_score_features = [i,j,k]
-            print(str(max_score))                    
+                clf = tree.DecisionTreeClassifier(random_state=1000000007,min_samples_split=2)
+                clf.fit(usefull_train_features,train_targets)
+                score = clf.score(usefull_test_features,test_targets)*100
+                #print(str(i)+" "+str(score))
+                if score > max_score:
+                    max_score = score
+                    max_score_features = [i,j,k,l]
+    print(str(max_score))                    
+    print(max_score_features)
 print(max_score)
 print(max_score_features)
